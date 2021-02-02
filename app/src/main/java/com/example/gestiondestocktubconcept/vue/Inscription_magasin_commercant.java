7bdbdf07ede01392e3c52e08377428694bc8c447
MichaelCazabat <74002393+MichaelCazabat@users.noreply.github.com>
2021-02-02 16:30:42 +0100

    version du 02/02/2021

---
+++ app/src/main/java/com/example/gestiondestocktubconcept/vue/Inscription_magasin_commercant.java
@@ -0,0 +1,16 @@
+package com.example.gestiondestocktubconcept.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gestiondestocktubconcept.R;
+
+public class Inscription_magasin_commercant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription_magasin_commercant);
+    }
+}
--- app/src/main/java/com/example/gestiondestocktubconcept/vue/Page_login_commercant.java
+++ app/src/main/java/com/example/gestiondestocktubconcept/vue/Page_login_commercant.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.gestiondestocktubconcept.R;
 import com.example.gestiondestocktubconcept.controleur.Controle;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Page_login_commercant extends AppCompatActivity {
 
-
+        String test = "2001";
         private Controle controle;
 
         @Override
@@ -22,22 +26,68 @@ public class Page_login_commercant extends AppCompatActivity {
                 setContentView(R.layout.connexion_commercant);
                 this.controle = controle.getInstance();
 
-        /* ++propriétées++ */
-        EditText txt_id_commercant;
-        EditText mdp_commercant;
-        Button btn_connexion_commercant = (Button) findViewById(R.id.btn_connexion_commercant);
+                /* ++propriétées++ */
+                EditText txt_id_commercant = findViewById(R.id.txt_id_commercant);
+                EditText mdp_commercant = findViewById(R.id.mdp_commercant);
+                Button btn_connexion_commercant = (Button) findViewById(R.id.btn_connexion_commercant);
 
-        /* --propriétées-- */
+                /* --propriétées-- */
 
 
                 btn_connexion_commercant.setOnClickListener(new View.OnClickListener(){
-
                         @Override
                         public void onClick(View v) {
-                                Intent l = new Intent(Page_login_commercant.this,liste_produits.class);
-                                startActivity(l);
+                                if (mdp_commercant.getText().toString().equals("azerty") && txt_id_commercant.getText().toString().equals("tubconcept")) {
+                                        Intent l = new Intent(Page_login_commercant.this, liste_produits.class);
+                                        startActivity(l);
+                                }else{
+                                        Toast.makeText(Page_login_commercant.this, "Vos identifiants sonts incorrects", Toast.LENGTH_SHORT).show();
+                                }
+
                         }
                 });
+
+
+                Toast.makeText(this, sha1Hash(test), Toast.LENGTH_SHORT).show();
         }
+
+        String sha1Hash( String toHash )
+        {
+                String hash = null;
+                try
+                {
+                        MessageDigest digest = MessageDigest.getInstance( "SHA-1" );
+                        byte[] bytes = toHash.getBytes("UTF-8");
+                        digest.update(bytes, 0, bytes.length);
+                        bytes = digest.digest();
+
+                        // This is ~55x faster than looping and String.formating()
+                        hash = bytesToHex( bytes );
+                }
+                catch( NoSuchAlgorithmException e )
+                {
+                        e.printStackTrace();
+                }
+                catch( UnsupportedEncodingException e )
+                {
+                        e.printStackTrace();
+                }
+                return hash;
+        }
+
+        // http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
+        final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+        public static String bytesToHex( byte[] bytes )
+        {
+                char[] hexChars = new char[ bytes.length * 2 ];
+                for( int j = 0; j < bytes.length; j++ )
+                {
+                        int v = bytes[ j ] & 0xFF;
+                        hexChars[ j * 2 ] = hexArray[ v >>> 4 ];
+                        hexChars[ j * 2 + 1 ] = hexArray[ v & 0x0F ];
+                }
+                return new String( hexChars );
+        }
+
 
 }
